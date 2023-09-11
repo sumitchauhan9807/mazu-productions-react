f985b3b82e393aec9e2c185950a6c288414362c1
Sumit chauhan <sumitchauhan9807666@gmail.com>
2023-09-11 12:56:14 +0530

    wo[

---
+++ src/components/Register/RegisterStepTwo.jsx
@@ -0,0 +1,11 @@
+import 'assets/css/custom/register.css'
+
+function RegisterStepTwo() {
+  return (
+    <div>
+      <h1 style={{backgroundColor:'red'}}>step2</h1>
+    </div>
+  )
+}
+
+export default RegisterStepTwo
