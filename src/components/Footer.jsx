d7af690c56c38ca94126eb80c917d8881106e9d4
Nilshan <nilshanalaguraj6@gmail.com>
2023-09-12 20:25:19 +0530

    footerfix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 function Footer () {
   return (
     <footer>
-    <div className="footer-top-wrap" style={{backgroundColor: 'black'}}>
+    <div className="footer-top-wrap bg-gray-900" >
       <div className="container">
         <div className="footer-menu-wrap">
           <div className="row align-items-center">
