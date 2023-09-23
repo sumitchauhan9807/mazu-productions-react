11a5e8b6f2180c584aedd25f390a3abde8c222e9
Sumit chauhan <sumitchauhan9807666@gmail.com>
2023-09-23 12:27:45 +0530

    wip

--- src/views/website/Login.jsx
+++ src/views/website/Login.jsx
@@ -149,10 +149,6 @@ return(
                          type="button" className="w-full flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600  hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-4  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500">
                       Sign in
                     </button>
-                    <Link   to="/admin"
-                          className="w-full flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600  hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-4  rounded-full tracking-wide font-semibold  shadow-lg cursor-pointer transition ease-in duration-500">
-                     sdsdsd
-                    </Link>
                   </div>
                 </form>
               </div>
