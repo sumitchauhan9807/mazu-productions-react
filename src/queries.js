890e33473e05d96903f71aeba2141f9db25e6c5d
Sumit chauhan <sumitchauhan9807666@gmail.com>
2023-10-17 16:22:39 +0530

    wip

--- src/queries.js
+++ src/queries.js
@@ -1,184 +1,228 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const GET_ALL_MEDIA_BY_CATEGORY = gql`
-query{
-  getAllMediaByCategory{
-    id
-    title
-    thumbnail
-    mediaCategory
+  query {
+    getAllMediaByCategory {
+      id
+      title
+      thumbnail
+      mediaCategory
+    }
   }
-}`
-
+`;
 
 export const GET_MEDIA_BY_CATEGORY = gql`
-query getMediaByCategory($category:String!){
-  getMediaByCategory(category:$category){
-    id
-    url
-    title
-    description
-    preview_url
-    thumbnail
-    potrait
+  query getMediaByCategory($category: String!) {
+    getMediaByCategory(category: $category) {
+      id
+      url
+      title
+      description
+      preview_url
+      thumbnail
+      potrait
+    }
   }
-}`
-
-
+`;
 
 export const GET_ALL_ACTOR_WEB_VIDEOS = gql`
-query getAllActorWebVideos($id:String!){
-  getAllActorWebVideos(id:$id){
-    id
-    source
-    cover
-    name
-    updates{
+  query getAllActorWebVideos($id: String!) {
+    getAllActorWebVideos(id: $id) {
       id
-      amount
-      views
+      source
+      cover
+      name
+      updates {
+        id
+        amount
+        views
+      }
     }
   }
-}`
+`;
 
 export const STRIP_CHAT_DATA = gql`
-query stripChatAPI($id:String!){
-  stripChatData(id:$id){
-    id
-    startDate
-    endDate
-    totalEarnings
+  query stripChatAPI($id: String!) {
+    stripChatData(id: $id) {
+     earnings{
+      id
+      startDate
+      endDate
+      totalEarnings
+      isPaid
+     }
+     actor {
+       username
+     }
+    }
   }
-}`
+`;
 
 export const GET_MODEL_BY_CATEGORY = gql`
-query getModelByCategory($preference:String!){
-  getModelByCategory(preference:$preference){
-    id
-    username
-    usrType
-    profilePic
-    looks{
-      preference
+  query getModelByCategory($preference: String!) {
+    getModelByCategory(preference: $preference) {
+      id
+      username
+      usrType
+      profilePic
+      looks {
+        preference
+      }
     }
   }
-}`
+`;
 
 export const GET_MODEL_DEFAULT_GALLERY = gql`
-query getModelDefaultGallery{
-  getModelDefaultGallery{
-    id
-    name
-    media{
+  query getModelDefaultGallery {
+    getModelDefaultGallery {
       id
-      cloudUrl
-      format
+      name
+      media {
+        id
+        cloudUrl
+        format
+      }
     }
   }
-}`
+`;
 
 export const GET_ACTOR_META = gql`
-query getActorMeta($id:String!){
-  getActorMeta(id:$id){
-    actorMeta{
-      stripChatUsername
+  query getActorMeta($id: String!) {
+    getActorMeta(id: $id) {
+      actorMeta {
+        stripChatUsername
+      }
     }
   }
-}`
+`;
 
 export const UPDATE_ACTOR_META = gql`
-mutation UpdateActorMeta($stripChatUsername:String!,$id:String!){
-  UpdateActorMeta(stripChatUsername:$stripChatUsername,id:$id){
-    actorMeta{
-      stripChatUsername
+  mutation UpdateActorMeta($stripChatUsername: String!, $id: String!) {
+    UpdateActorMeta(stripChatUsername: $stripChatUsername, id: $id) {
+      actorMeta {
+        stripChatUsername
+      }
     }
   }
-}`
+`;
 
-
-export const USER_REGISTER = gql `
-    mutation register($options:UsernamePasswordInput!,$addr: String!,$userType:String!,$preference:String!,$modelData:ModelRegisterFields,$modelAff:String){
-        register(options:$options, addr:$addr,userType:$userType,preference:$preference,modelData:$modelData,modelAff:$modelAff,){
-            errors{
-                field
-                message
-            }
-            user{
-              profilePic
-                username
-                email
-                usrType
-                profileSetupStep
-                profileComplete
-                photos {
-                    id
-                    image
-                    unsafe
-                }
-                base_profile {
-                    id
-                    firstName
-                    lastName
-                }
-                user_basic{
-                  location
-                }
-            }
-            credit{
-                id
-                balance
-                flirtons
-                flirtonsAvailable
-            }
-            token
+export const USER_REGISTER = gql`
+  mutation register(
+    $options: UsernamePasswordInput!
+    $addr: String!
+    $userType: String!
+    $preference: String!
+    $modelData: ModelRegisterFields
+    $modelAff: String
+  ) {
+    register(
+      options: $options
+      addr: $addr
+      userType: $userType
+      preference: $preference
+      modelData: $modelData
+      modelAff: $modelAff
+    ) {
+      errors {
+        field
+        message
+      }
+      user {
+        profilePic
+        username
+        email
+        usrType
+        profileSetupStep
+        profileComplete
+        photos {
+          id
+          image
+          unsafe
         }
+        base_profile {
+          id
+          firstName
+          lastName
+        }
+        user_basic {
+          location
+        }
+      }
+      credit {
+        id
+        balance
+        flirtons
+        flirtonsAvailable
+      }
+      token
     }
-`
+  }
+`;
 
-export const UPDATE_PROFILE_PIC = gql `mutation updateProfilePic($file: Upload!) {
-  updateProfilePic(file: $file)
-}
-`
+export const UPDATE_PROFILE_PIC = gql`
+  mutation updateProfilePic($file: Upload!) {
+    updateProfilePic(file: $file)
+  }
+`;
 
-export const CREATE_TEAM = gql `mutation createTeam($name: String!) {
-  createTeam(name: $name){
-    name
-    recuiter{
+export const RELEASE_STRIPCHAT_PAYMENT = gql`mutation releaseStripchatPayment($id:String!,$startDate:DateTime!,$endDate:DateTime!) {
+  releaseStripchatPayment(id: $id,startDate:$startDate,endDate:$endDate){
+    payout{
       id
+      amount
     }
-    manager{
+    paidEarnings{
       id
+      totalEarnings
     }
   }
 }
-`
+`;
+
+export const CREATE_TEAM = gql`
+  mutation createTeam($name: String!) {
+    createTeam(name: $name) {
+      name
+      recuiter {
+        id
+      }
+      manager {
+        id
+      }
+    }
+  }
+`;
 export const GET_CLOUD_PUT_URL = gql`
-  query getCloudPutUrl($filename: String!,$mimetype:String!,$bucket:String) {
-    getCloudPutUrl(filename: $filename,mimetype:$mimetype,bucket:$bucket) 
+  query getCloudPutUrl(
+    $filename: String!
+    $mimetype: String!
+    $bucket: String
+  ) {
+    getCloudPutUrl(filename: $filename, mimetype: $mimetype, bucket: $bucket)
   }
 `;
 export const ADD_MODEL_GALLERY_MEDIA = gql`
   mutation addModelMedia(
     $cost: Float!
     $is_paid: Boolean!
-    $mediaCloudName:String!
-    $mimetype:String!
+    $mediaCloudName: String!
+    $mimetype: String!
   ) {
     addModelMedia(
       cost: $cost
       is_paid: $is_paid
-      mediaCloudName:$mediaCloudName
-      mimetype:$mimetype
-    ){
-       id
-       url
-       cost
-       format
-       mediaCloudName
-       format
-       is_paid
-       cloudUrl
-     }
+      mediaCloudName: $mediaCloudName
+      mimetype: $mimetype
+    ) {
+      id
+      url
+      cost
+      format
+      mediaCloudName
+      format
+      is_paid
+      cloudUrl
+    }
   }
 `;
 
@@ -186,213 +230,247 @@ export const ADD_MODEL_WEB_VIDEO = gql`
   mutation addWebVideo(
     $source: String!
     $id: String!
-    $name:String!
-    $cover:String!
+    $name: String!
+    $cover: String!
   ) {
-    addWebVideo(
-      source: $source
-      id: $id
-      name:$name
-      cover:$cover
-    ){
-       id
-       source
-     }
+    addWebVideo(source: $source, id: $id, name: $name, cover: $cover) {
+      id
+      source
+    }
   }
 `;
 
-export const BASIC_SELCETIONS = gql `query{
-  basicInfoSelections{
-    hairColorTypes
-    genders
-    preferences
+export const BASIC_SELCETIONS = gql`
+  query {
+    basicInfoSelections {
+      hairColorTypes
+      genders
+      preferences
+    }
   }
-}`
+`;
 
-export const GET_ALL_TEAMS = gql `query{
-  getAllTeams {
-    id
-    name
-    admin{
+export const GET_ALL_TEAMS = gql`
+  query {
+    getAllTeams {
       id
-      username
       name
-    }
-  }
-}`
-
-export const GET_TEAM_MANAGERS = gql `query getTeamManagers($id:String!) {
-  getTeamManagers(id:$id) {
-    manager{
-      id
-      username
-      name
-      email
-    }
-    team{
-      name
-    }
-  }
-}`
-
-export const GET_TEAM_ADMINS = gql `query getAllTeamManagers{
-  getAllTeamManagers {
-    id
-    username
-    name
-    email
-  }
-}`
-
-
-export const GET_MANAGER_RECUITERS = gql `query getManagerRecuiters($id:String!) {
-  getManagerRecuiters(id:$id) {
-    recuiter{
-      id
-      username
-      name
-      email
-    }
-    manager{
-      name
-      username
-    }
-  }
-}`
-
-export const ADD_MANAGER = gql `mutation addManager(
-  $name:String!
-  $password:String!,
-  $email:String!,
-  $team:String!,
-  $username:String!
-){
-  addManager(name:$name,password:$password,email:$email,team:$team,username:$username){
-    id
-    username
-    email
-  }
-}`
-
-export const ADD_TEAM_ADMIN = gql `mutation addTeamAdmin(
-  $name:String!
-  $password:String!,
-  $email:String!,
-  $username:String!
-){
-  addTeamAdmin(name:$name,password:$password,email:$email,username:$username){
-    id
-    username
-    email
-  }
-}`
-
-export const UPDATE_TEAM_ADMIN = gql `mutation updateTeamAdmin(
-  $teamId:String!
-  $adminId:String!,
-){
-  updateTeamAdmin(teamId:$teamId,adminId:$adminId)
-}`
-
-
-
-export const ADD_RECUITER = gql `mutation addRecuiter(
-  $name:String!
-  $password:String!,
-  $email:String!,
-  $manager:String!,
-  $username:String!
-){
-  addRecuiter(name:$name,password:$password,email:$email,manager:$manager,username:$username){
-    id
-    username
-    email
-  }
-}`
-
-export const GET_ALL_ACTORS = gql`query{
-  getAllActors{
-    id
-    username
-    profilePic
-    recuiter{
-      username
-    }
-  }
-}`
-export const ADD_BASIC_INFO = gql `mutation addBasicInformation($options:BasicInfoInput!){
-  addBasicInformation(options:$options)
-}`
-export const VERIFY_OTP = gql `mutation verifyOtp($otp:String!,$MP:Boolean){
-  verifyOtp(otp:$otp,MP:$MP){
-      user{            
-          profilePic
-          username
-          email
-          age
-          usrType
-          profileSetupStep
-          profileComplete
-          photos {
-              id
-              image
-              unsafe
-          }
-          base_profile {
-              id
-              firstName
-              lastName
-          }
-          user_basic{
-            location
-          }
+      admin {
+        id
+        username
+        name
       }
-      credit{
+    }
+  }
+`;
+
+export const GET_TEAM_MANAGERS = gql`
+  query getTeamManagers($id: String!) {
+    getTeamManagers(id: $id) {
+      manager {
+        id
+        username
+        name
+        email
+      }
+      team {
+        name
+      }
+    }
+  }
+`;
+
+export const GET_TEAM_ADMINS = gql`
+  query getAllTeamManagers {
+    getAllTeamManagers {
+      id
+      username
+      name
+      email
+    }
+  }
+`;
+
+export const GET_MANAGER_RECUITERS = gql`
+  query getManagerRecuiters($id: String!) {
+    getManagerRecuiters(id: $id) {
+      recuiter {
+        id
+        username
+        name
+        email
+      }
+      manager {
+        name
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_MANAGER = gql`
+  mutation addManager(
+    $name: String!
+    $password: String!
+    $email: String!
+    $team: String!
+    $username: String!
+  ) {
+    addManager(
+      name: $name
+      password: $password
+      email: $email
+      team: $team
+      username: $username
+    ) {
+      id
+      username
+      email
+    }
+  }
+`;
+
+export const ADD_TEAM_ADMIN = gql`
+  mutation addTeamAdmin(
+    $name: String!
+    $password: String!
+    $email: String!
+    $username: String!
+  ) {
+    addTeamAdmin(
+      name: $name
+      password: $password
+      email: $email
+      username: $username
+    ) {
+      id
+      username
+      email
+    }
+  }
+`;
+
+export const UPDATE_TEAM_ADMIN = gql`
+  mutation updateTeamAdmin($teamId: String!, $adminId: String!) {
+    updateTeamAdmin(teamId: $teamId, adminId: $adminId)
+  }
+`;
+
+export const ADD_RECUITER = gql`
+  mutation addRecuiter(
+    $name: String!
+    $password: String!
+    $email: String!
+    $manager: String!
+    $username: String!
+  ) {
+    addRecuiter(
+      name: $name
+      password: $password
+      email: $email
+      manager: $manager
+      username: $username
+    ) {
+      id
+      username
+      email
+    }
+  }
+`;
+
+export const GET_ALL_ACTORS = gql`
+  query {
+    getAllActors {
+      id
+      username
+      profilePic
+      recuiter {
+        username
+      }
+    }
+  }
+`;
+export const ADD_BASIC_INFO = gql`
+  mutation addBasicInformation($options: BasicInfoInput!) {
+    addBasicInformation(options: $options)
+  }
+`;
+export const VERIFY_OTP = gql`
+  mutation verifyOtp($otp: String!, $MP: Boolean) {
+    verifyOtp(otp: $otp, MP: $MP) {
+      user {
+        profilePic
+        username
+        email
+        age
+        usrType
+        profileSetupStep
+        profileComplete
+        photos {
           id
-          balance
-          flirtons
-          flirtonsAvailable
+          image
+          unsafe
+        }
+        base_profile {
+          id
+          firstName
+          lastName
+        }
+        user_basic {
+          location
+        }
       }
-  }
-}`
-export const USER_LOGIN = gql `
-mutation login($usernameOrEmail: String!, $password: String!, $addr: String!) {
-    login(usernameOrEmail: $usernameOrEmail, password: $password, addr:$addr) {
-        errors {
-            field
-            message
-        }
-        token
-        user {
-            authSource
-            profilePic
-            username
-            email
-            profileSetupStep
-            profileComplete
-            photos {
-                id
-                image
-                unsafe
-            }
-            base_profile {
-                id
-                firstName
-                lastName
-            }
-            user_basic{
-              location
-            }
-        }
-        credit{
-            id
-            balance
-            flirtons
-            flirtonsAvailable
-        }
+      credit {
+        id
+        balance
+        flirtons
+        flirtonsAvailable
+      }
     }
-}
-`
+  }
+`;
+export const USER_LOGIN = gql`
+  mutation login(
+    $usernameOrEmail: String!
+    $password: String!
+    $addr: String!
+  ) {
+    login(usernameOrEmail: $usernameOrEmail, password: $password, addr: $addr) {
+      errors {
+        field
+        message
+      }
+      token
+      user {
+        authSource
+        profilePic
+        username
+        email
+        profileSetupStep
+        profileComplete
+        photos {
+          id
+          image
+          unsafe
+        }
+        base_profile {
+          id
+          firstName
+          lastName
+        }
+        user_basic {
+          location
+        }
+      }
+      credit {
+        id
+        balance
+        flirtons
+        flirtonsAvailable
+      }
+    }
+  }
+`;
 export const MOD_QUERY = gql`
   mutation modLogin(
     $password: String!
@@ -430,7 +508,7 @@ export const GET_ACTOR_MEDIA_ADMIN = gql`
           cloudUrl
         }
       }
-      user{
+      user {
         username
       }
     }
--- src/views/Admin/StripChat.jsx
+++ src/views/Admin/StripChat.jsx
@@ -1,5 +1,5 @@
 import { useEffect,useState } from "react"
-import {STRIP_CHAT_DATA} from 'queries'
+import {STRIP_CHAT_DATA,RELEASE_STRIPCHAT_PAYMENT} from 'queries'
 import {apolloClient} from 'index'
 import {useParams} from 'react-router-dom';
 import Loader from 'components/UI/Loader'
@@ -8,18 +8,26 @@ import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
 
-
+import Swal from "sweetalert2"
 function StripChat() {
-
 let [isLoading,setLoading] = useState(false)
 let [meta,setMeta] = useState({
   stripChatUsername:"",
 })
 const params = useParams();
 const alertUser = useAlert()
-let [stripChatData,setStripData] = useState([])
-const [startDate, setStartDate] = useState(new Date('2023-10-01'));
+let [stripChatData,setStripData] = useState({
+  earnings:[],
+  actor:null
+})
+const [stripchatAmount, setStripchatAmount] = useState({
+  amountPaid:0,
+  unpaidAmount:0,
+  totalAmount:0
+});
+const [startDate, setStartDate] = useState(new Date());
 const [endDate, setEndDate] = useState(new Date());
+const [apiCount,setCount] = useState(0)
 
 
 
@@ -34,34 +42,82 @@ const [endDate, setEndDate] = useState(new Date());
           },
           fetchPolicy:'no-cache'
         })
-        setStripData(data.stripChatData)
-       
+        if(data.stripChatData) {
+          setStripData(data.stripChatData)
+          setStripchatAmount(()=>{
+            return {
+              amountPaid:data.stripChatData.earnings.reduce((acc,currentValue)=>{
+                let ret = currentValue.isPaid ? Number(currentValue.totalEarnings) + acc : acc
+                return ret
+              },0),
+              unpaidAmount:data.stripChatData.earnings.reduce((acc,currentValue)=>{
+                let ret = !currentValue.isPaid ? Number(currentValue.totalEarnings) + acc : acc
+                return ret
+              },0),
+              totalAmount:data.stripChatData.earnings.reduce((acc,currentValue)=>{
+                return Number(currentValue.totalEarnings) + acc 
+              },0)
+            }
+          })
+        }
        setLoading(false)
-
       }catch(e) {
         setLoading(false)
         alertUser.error(e.message)
       }
     })()
-  },[startDate,endDate])
+  },[apiCount])
 
   useEffect(()=>{
     console.log(stripChatData,"stripChatData")
   },[stripChatData])
 
+  const releasePayment = async (startDate,endDate) => {
+    console.log(startDate)
+    console.log(endDate)
+    let data = await new Swal({
+      title: "Are you sure?",
+      text: "Payments will be release between this period!",
+      type: "confirm",
+      showCancelButton: true,
+      confirmButtonColor: '#DD6B55',
+      confirmButtonText: 'Yes, I am sure!',
+      cancelButtonText: "No, cancel it!",
+    })
+    console.log(data)
+    if(!data.isConfirmed) return 
+    try {
+      setLoading(true)
+      let {data,errors} = await apolloClient.query({
+        query: RELEASE_STRIPCHAT_PAYMENT,
+        variables: {
+          id:params.id,
+          startDate:startDate,
+          endDate:endDate
+        },
+        fetchPolicy:'no-cache'
+      })
+      if(data.releaseStripchatPayment) {
+        setCount((prev)=>{
+          return prev + 1
+        })
+      }
+      setLoading(false)
+    }catch(e) {
+      setLoading(false)
+      alertUser.error(e.message)
+    }
+  }
+
   
   return (
     <div className="container w-full mx-auto pt-20">
         { isLoading && <Loader/>}
-      <div className="w-full px-4 md:px-0 md:mt-8  text-gray-800 leading-normal">
-        <div className="p-4 justify-center items-center">
-      <div className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      {/* Start date  <DatePicker onChange={setStartDate} value={startDate} /> &nbsp;
-      End Date  <DatePicker  onChange={setEndDate} value={endDate} />
-      <br/>
-      <br/>
-    <button  className="w-1/4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Filter</button> */}
-      </div>
+        
+        
+    <div className="w-full px-4 md:px-0 md:mt-8  text-gray-800 leading-normal">
+      <div className="p-4 justify-center items-center pb-4">
+      <h3 className="sm:text-3xl text-xl font-medium title-font mb-4 text-gray-900">Stripchat Earnings of {stripChatData?.actor?.username}</h3>
       <div className="relative overflow-x-auto mt-12">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -75,10 +131,13 @@ const [endDate, setEndDate] = useState(new Date());
               <th scope="col" className="px-6 py-3">
                 End Date
               </th>
+              <th scope="col" className="px-6 py-3">
+                Release Payment
+              </th>
             </tr>
           </thead>
           <tbody>
-           {stripChatData.map((stripData)=>{
+           {stripChatData.earnings.map((stripData)=>{
              return (
               <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -90,6 +149,9 @@ const [endDate, setEndDate] = useState(new Date());
               <td className="px-6 py-4">
                {formatDate(stripData.endDate)}
               </td>
+              <td className="px-6 py-4">
+              {!stripData.isPaid && <button onClick={() => { releasePayment(formatDate(stripData.startDate),formatDate(stripData.endDate)) }} className="w-1/4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Release Payment</button> }
+              </td>
             </tr>
              )
            })}
@@ -97,6 +159,22 @@ const [endDate, setEndDate] = useState(new Date());
         </table>
           </div>
         </div>
+      </div>
+      <div className="w-full text-sm text-left text-gray-500 dark:text-gray-400  p-4">
+        <span className="font-bold uppercase text-gray-500 mb-4">Unpaid Amount : {stripchatAmount.unpaidAmount}</span> <br/>
+        <span className="font-bold uppercase text-gray-500">Paid Amount : {stripchatAmount.amountPaid}</span><br/>
+        <span className="font-bold uppercase text-gray-500 mb-4">Total Amount Earned  : {stripchatAmount.totalAmount}</span> 
+      </div>
+      <div className="w-full text-sm text-left text-gray-500 dark:text-gray-400 mb-32 p-4">
+      <hr className="mb-6"/>
+
+      <h5 className="font-bold uppercase text-gray-500 mb-4">Release payment for a period</h5>
+
+      Start date  <DatePicker onChange={setStartDate} value={startDate} /> &nbsp;
+      End Date  <DatePicker  onChange={setEndDate} value={endDate} />
+      <br/>
+      <br/>
+    <button onClick={() => releasePayment(formatDate(startDate),formatDate(endDate)) } className="w-1/4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Release Payment</button>
       </div>
     </div>
   )
