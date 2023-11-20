96c021ebb6bdce76ecb457795c08f89eea8161cf
Nilshan <nilshanalaguraj6@gmail.com>
2023-11-20 18:20:01 +0530

    udpate

--- src/views/website/Success.jsx
+++ src/views/website/Success.jsx
@@ -9,7 +9,7 @@ export const Success = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="text-green-600 w-28 h-28" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
               <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <h1 className="text-4xl font-bold">Pyament Successfull !</h1>
+            <h1 className="text-4xl font-bold">Payment Successful!</h1>
             <p className='text-center'>Thank you for your Payment! Check your email for a link to the guide.</p>
            
              
